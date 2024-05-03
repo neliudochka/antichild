c4df36f7a2d7505c62c50a470a8bd45e6c1b0242
Mila <gorbunova.milasha@gmail.com>
2024-05-03 18:32:01 +0300

    add on/off sensor button

--- app/src/main/java/com/example/antichild/MainActivity.kt
+++ app/src/main/java/com/example/antichild/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.antichild
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.antichild.databinding.ActivityMainBinding
 import com.example.antichild.sensors.AccelerometerSensor
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,8 +16,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initSensor()
-        startAccelerometer()
+        setButtonListeners()
     }
 
     override fun onDestroy() {
@@ -23,8 +24,37 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
+    //ui
+    private var isSensors = false;
+    private var greyColor by Delegates.notNull<Int>()
+    private var pinky by Delegates.notNull<Int>()
+    private var red by Delegates.notNull<Int>()
+
+    fun setButtonListeners() {
+        greyColor = applicationContext.getColor(R.color.gray)
+        pinky = applicationContext.getColor(R.color.pinky)
+        red = applicationContext.getColor(R.color.red)
+
+        binding.onOffSensors.setBackgroundColor(greyColor)
+        binding.onOffSensors.setOnClickListener {
+            switchOffSensors()
+        }
+    }
+
+    private fun switchOffSensors() {
+        if(isSensors) {
+            isSensors = false
+            binding.onOffSensors.setBackgroundColor(greyColor)
+            killSensors()
+        } else {
+            isSensors = true
+            binding.onOffSensors.setBackgroundColor(pinky)
+            initSensors()
+        }
+    }
+
     //функції роботи з сенсором
-    private fun initSensor() {
+    private fun initSensors() {
         accelerometerSensor = AccelerometerSensor(applicationContext)
         startAccelerometer()
     }
