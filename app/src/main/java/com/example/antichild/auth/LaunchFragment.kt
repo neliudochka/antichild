ecf403550d4832e5ccb3f235ba01baf1e5f4139d
svitbar <svitbar1102@gmail.com>
2024-05-18 23:25:04 +0300

    Create LaunchFragment

---
+++ app/src/main/java/com/example/antichild/auth/LaunchFragment.kt
@@ -0,0 +1,27 @@
+package com.example.antichild.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.antichild.R
+import com.example.antichild.databinding.FragmentLaunchBinding
+
+class LaunchFragment : Fragment() {
+    private lateinit var binding: FragmentLaunchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLaunchBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = LaunchFragment()
+    }
+}
