eb0222af4c806b4454f633909d6d9a3417cdc37a
Mila <gorbunova.milasha@gmail.com>
2024-05-28 21:58:57 +0300

    initialized models with empty strings.
    Without it there is an error during serialization.

--- app/src/main/java/com/example/antichild/models/User.kt
+++ app/src/main/java/com/example/antichild/models/User.kt
@@ -1,24 +1,24 @@
 package com.example.antichild.models
 
 open class User (
-    var uid: String,
-    var username: String,
-    var email: String,
-    var role: String,
+    var uid: String = "",
+    var username: String = "",
+    var email: String = "",
+    var role: String = "",
 )
 
 class Parent (
-    uid: String,
-    username: String,
-    email: String,
-    role: String,
-    var accessPassword: String,
+    uid: String = "",
+    username: String = "",
+    email: String = "",
+    role: String = "",
+    var accessPassword: String = "",
 ) : User(uid, username, email, role)
 
 class Child (
-    uid: String,
-    username: String,
-    email: String,
-    role: String,
-    var parentEmail: String
+    uid: String = "",
+    username: String = "",
+    email: String = "",
+    role: String = "",
+    var parentEmail: String = ""
 ) : User (uid, username, email, role)
