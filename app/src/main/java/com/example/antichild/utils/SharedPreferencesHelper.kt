c4793c70783d3b3bb62a8796e4c62a860aee2a49
svitbar <svitbar1102@gmail.com>
2024-06-04 01:37:54 +0300

    Modify save data methods to save parent data within child's

--- app/src/main/java/com/example/antichild/utils/SharedPreferencesHelper.kt
+++ app/src/main/java/com/example/antichild/utils/SharedPreferencesHelper.kt
@@ -2,14 +2,20 @@ package com.example.antichild.utils
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.example.antichild.models.Child
+import com.example.antichild.models.Parent
+import com.example.antichild.models.User
 
 object SharedPreferencesHelper {
     private const val PREFS_NAME = "user_prefs"
-    private const val KEY_UID = "uid"
-    private const val KEY_USERNAME = "username"
-    private const val KEY_EMAIL = "email"
-    private const val KEY_ROLE = "role"
+    private const val KEY_CHILD_UID = "uid"
+    private const val KEY_CHILD_USERNAME = "username"
+    private const val KEY_CHILD_EMAIL = "email"
     private const val KEY_ADVANCE = "advance"
+    private const val KEY_PARENT_UID = "parentUid"
+    private const val KEY_PARENT_NAME = "parentName"
+    private const val KEY_PARENT_EMAIL = "parentEmail"
+    private const val KEY_PARENT_ACCESS_PASSWORD = "accessPassword"
 
     private lateinit var sharedPreferences: SharedPreferences
 
@@ -17,24 +23,44 @@ object SharedPreferencesHelper {
         sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
     }
 
-    fun saveUserData(uid: String, username: String, email: String, role: String, advance: String) {
+    fun saveParentUserData(parent: Parent) {
         with(sharedPreferences.edit()) {
-            putString(KEY_UID, uid)
-            putString(KEY_USERNAME, username)
-            putString(KEY_EMAIL, email)
-            putString(KEY_ROLE, role)
-            putString(KEY_ADVANCE, advance)
+            putString(KEY_PARENT_UID, parent.uid)
+            putString(KEY_PARENT_NAME, parent.username)
+            putString(KEY_PARENT_EMAIL, parent.email)
+            putString(KEY_PARENT_ACCESS_PASSWORD, parent.accessPassword)
             apply()
         }
     }
 
-    fun getUserData(): UserData {
-        return UserData(
-            sharedPreferences.getString(KEY_UID, null),
-            sharedPreferences.getString(KEY_USERNAME, null),
-            sharedPreferences.getString(KEY_EMAIL, null),
-            sharedPreferences.getString(KEY_ROLE, null),
-            sharedPreferences.getString(KEY_ADVANCE, null)
+    fun saveChildUserData(child: Child, parent: Parent) {
+        saveParentUserData(parent)
+        with(sharedPreferences.edit()) {
+            putString(KEY_CHILD_UID, child.uid)
+            putString(KEY_CHILD_USERNAME, child.username)
+            putString(KEY_CHILD_EMAIL, child.email)
+            apply()
+        }
+    }
+
+    fun getChildUserData(): ChildData {
+        return ChildData(
+            sharedPreferences.getString(KEY_CHILD_UID, null),
+            sharedPreferences.getString(KEY_CHILD_USERNAME, null),
+            sharedPreferences.getString(KEY_CHILD_EMAIL, null),
+            sharedPreferences.getString(KEY_PARENT_UID, null),
+            sharedPreferences.getString(KEY_PARENT_NAME, null),
+            sharedPreferences.getString(KEY_PARENT_EMAIL, null),
+            sharedPreferences.getString(KEY_PARENT_ACCESS_PASSWORD, null)
+        )
+    }
+
+    fun getParentUserData(): ParentData {
+        return ParentData(
+            sharedPreferences.getString(KEY_PARENT_UID, null),
+            sharedPreferences.getString(KEY_PARENT_NAME, null),
+            sharedPreferences.getString(KEY_PARENT_EMAIL, null),
+            sharedPreferences.getString(KEY_PARENT_ACCESS_PASSWORD, null)
         )
     }
 
