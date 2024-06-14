9d62a4960adb738123566de487818231d01b0abf
svitbar <svitbar1102@gmail.com>
2024-06-14 20:49:05 +0300

    Implement direct reply handling

--- app/src/main/java/com/example/antichild/notification/ButtonActionReceiver.kt
+++ app/src/main/java/com/example/antichild/notification/ButtonActionReceiver.kt
@@ -1,14 +1,33 @@
 package com.example.antichild.notification
 
+import android.app.NotificationManager
+import android.app.RemoteInput
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import android.widget.Toast
+import com.example.antichild.utils.SharedPreferencesHelper
 
 class ButtonActionReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action == "com.example.antichild.ACTION_REPLY") {
-            Log.d("ButtonActionReceiver", "Message received!")
+            val remoteInput = RemoteInput.getResultsFromIntent(intent)
+            val remoteText = remoteInput.getCharSequence(MotionAlarmNotification.KEY_TEXT_REPLY).toString()
+
+            if (validatePassword(remoteText)) {
+                // send to child
+
+                val notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.cancel(intent.getIntExtra("notification_id", 0))
+            } else {
+                Toast.makeText(context, "Wrong password!", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun validatePassword(password: String): Boolean {
+        return if (password.isEmpty()) {
+            false
+        } else password == SharedPreferencesHelper.getParentData().accessPassword
     }
 }
